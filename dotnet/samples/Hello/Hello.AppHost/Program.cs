1f0749650e717bfc4c53345bf9a48a57da10cd1c
Reuben Bond <reuben.bond@gmail.com>
2024-11-08 13:36:27 -0800

    Print backend URL from .NET Hello.AppHost

--- dotnet/samples/Hello/Hello.AppHost/Program.cs
+++ dotnet/samples/Hello/Hello.AppHost/Program.cs
@@ -1,7 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Program.cs
 
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 var backend = builder.AddProject<Projects.Backend>("backend");
 builder.AddProject<Projects.HelloAgent>("client").WithReference(backend).WaitFor(backend);
-builder.Build().Run();
+
+using var app = builder.Build();
+
+await app.StartAsync();
+var url = backend.GetEndpoint("https").Url;
+Console.WriteLine("Backend URL: " + url);
+
+await app.WaitForShutdownAsync();

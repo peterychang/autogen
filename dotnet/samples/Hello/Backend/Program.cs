a4399e1d40d76cf90f2a42a05229dfd2b334750b
Reuben Bond <reuben.bond@gmail.com>
2024-11-08 14:15:57 -0800

    Reuben fixes

--- dotnet/samples/Hello/Backend/Program.cs
+++ dotnet/samples/Hello/Backend/Program.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Program.cs
 
-var app = await Microsoft.AutoGen.Runtime.Host.StartAsync(local: true);
+var app = await Microsoft.AutoGen.Runtime.Host.StartAsync(local: false);
 await app.WaitForShutdownAsync();
